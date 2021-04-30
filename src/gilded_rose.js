f6d57cbd419492789aacc2b7f1ba27e90f50019f
Viviane Dias <vivianedias@protonmail.com>
2021-04-30 16:17:04 -0300

    refactor: changes conditions to be positive

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,9 +6,9 @@ class Item {
   }
 }
 
-const specialItens = ["Sulfuras", "Aged Brie", "Backstage Passes"]
+const specialItens = ["Sulfuras", "Aged Brie", "Backstage Passes"];
 
-const isSpecialItem = (name) => specialItens.includes(name)
+const isSpecialItem = (name) => specialItens.includes(name);
 
 class Shop {
   constructor(items = []) {
@@ -16,14 +16,12 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      const { name, quality } = this.items[i]
+      const { name, quality } = this.items[i];
 
-      if (!isSpecialItem(name) && quality > 0) {
-        this.items[i].quality = this.items[i].quality - 1;
-      } else {
+      if (isSpecialItem(name) && quality > 0) {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage Passes") {
+          if (name == "Backstage Passes") {
             if (this.items[i].sellIn < 11) {
               this.items[i].quality = this.items[i].quality + 1;
             }
@@ -32,26 +30,29 @@ class Shop {
             }
           }
         }
+      } else {
+        this.items[i].quality = this.items[i].quality - 1;
       }
 
-      if (this.items[i].name != "Sulfuras") {
+      if (name !== "Sulfuras") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage Passes") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras") {
+        if (name === "Aged Brie") {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        } else {
+          if (name === "Backstage Passes") {
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
+          } else {
+            if (name != "Sulfuras") {
+              if (this.items[i].quality > 0) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
