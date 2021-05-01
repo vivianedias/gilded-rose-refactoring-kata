3f9c47a7b111555535ff0f5a6c136e07cc473b41
Viviane Dias <vivianedias@protonmail.com>
2021-05-01 18:20:55 -0300

    Migrates funcs and vars inside Shop class to use OOP

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,57 +6,79 @@ class Item {
   }
 }
 
-const specialItens = ["Sulfuras", "Aged Brie", "Backstage Passes"];
-
-const isSpecialItem = (name) => specialItens.includes(name);
-
 class Shop {
   constructor(items = []) {
     this.items = items;
+    this.specialItens = [
+      "Sulfuras",
+      "Aged Brie",
+      "Backstage Passes",
+      "Conjured",
+    ];
+  }
+
+  isSpecialItem(name) {
+    return this.specialItens.includes(name);
   }
 
   updateQuality() {
     const items = this.items.map(({ name, sellIn, quality }) => {
+      if (name === "Sulfuras")
+        return {
+          name,
+          sellIn,
+          quality,
+        };
+
       let newSellIn = sellIn;
       let newQuality = quality;
 
-      if (name === "Sulfuras") return {
+      newSellIn = newSellIn - 1;
+
+      const sellInAndName = {
+        sellIn: newSellIn,
         name,
-        sellIn,
-        quality
       };
 
-      newSellIn = newSellIn - 1
-
       if (newQuality > 0 && newQuality < 50) {
-        if(!isSpecialItem(name)) {
+        if (!this.isSpecialItem(name)) {
           newQuality = newQuality - 1;
-          if (newSellIn < 0) {
-            newQuality = newQuality - 1
+
+          if (newSellIn < 0 && newQuality > 0) {
+            newQuality = newQuality - 1;
           }
-        } else {
-          newQuality = newQuality + 1;
+
+          return {
+            quality: newQuality,
+            ...sellInAndName,
+          };
         }
+
+        newQuality = newQuality + 1;
       }
 
       if (name === "Backstage Passes") {
+        if (newSellIn < 0) {
+          return {
+            quality: newQuality - newQuality,
+            ...sellInAndName,
+          };
+        }
+
         if (newSellIn <= 10 && newQuality < 50) {
           newQuality = newQuality + 1;
         }
+
         if (newSellIn <= 5 && newQuality < 50) {
           newQuality = newQuality + 1;
         }
-        if (newSellIn < 0) {
-          newQuality = newQuality - newQuality;
-        }
       }
-      
+
       return {
-        sellIn: newSellIn,
-        name,
-        quality: newQuality
-      }
-    })
+        quality: newQuality,
+        ...sellInAndName,
+      };
+    });
 
     this.items = items;
 
